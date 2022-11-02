449020faccabef14b04a04a852313eab22a1e2aa
mariana73154 <102183199+mariana73154@users.noreply.github.com>
2022-11-02 14:46:10 +0000

    Delete FP20_21_v1 directory

--- FP20_21_v1/FP20_21_v1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FP20_21_v1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
